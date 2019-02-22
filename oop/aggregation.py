6dbe085d6620de4c4746ce6d98abe93e5c12cfca
Vitaliy Krasnov <krasnov.v.i@gmail.com>
2019-02-22 17:37:51 +0300

    Added OOP dir

---
+++ oop/aggregation.py
@@ -0,0 +1,29 @@
+"""
+Ассоциация описывается словом «имеет». Автомобиль имеет двигатель. Вполне естественно, что он не будет являться наследником двигателя (хотя такая архитектура тоже возможна в некоторых ситуациях).
+Аггрегация - Разновидность ассоциации, экземпляр класса создается вне и передается в конструктор аргументом.
+"""
+
+
+class Engine:
+    def __init__(self, power: int):
+        self.power = power
+
+    def get_power(self) -> int:
+        return self.power
+
+
+class Car:
+    def __init__(self, engine: object, model: str):
+        self.engine = engine
+        self.model = model
+
+    def get_model(self) -> str:
+        return self.model
+
+
+if __name__ == "__main__":
+    # Созадем объект класса двигатель
+    engine = Engine(power=350)
+
+    # Создаем объект класса машина передавая в конструктор объект двигатель
+    car = Car(engine=engine, model='audi')
---
+++ oop/inherit.py
@@ -0,0 +1,32 @@
+"""
+Наследование - дочерний класс наследует все поля и методы родительского класса.
+"""
+
+
+class Shape:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def move(self, new_x: int, new_y: int):
+        self.x = new_x
+        self.y = new_y
+
+    def get_position(self) -> tuple:
+        return self.x, self.y
+
+
+class Circle(Shape):
+    def __init__(self, x: int, y: int, r: int):
+        super(Circle, self).__init__(x, y)
+        self.r = r
+
+    def get_radius(self) -> int:
+        return self.r
+
+
+if __name__ == "__main__":
+    circle = Circle(x=5, y=6, r=10)
+    print("Circle position: {}".format(circle.get_position()))
+    print("Circle radius: {}".format(circle.get_radius()))
+
